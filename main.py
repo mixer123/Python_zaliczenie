9e5e1ee73fe8101edef1ad06534b5b6934fc429f
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2022-12-16 12:27:39 +0100

    8 commit

--- main.py
+++ main.py
@@ -8,21 +8,24 @@ import random
 
 def read_file(filename,header = True, sep=','):
     header_list = []
+    try:
+        with open(filename, mode ='r') as file:
+            lines = file.readlines()
+            if header:
+                header_list.append(lines[0].replace('\n',''))
+                return header_list,filename,sep, header
+        return header_list, filename, sep, header
+    except (FileNotFoundError, IOError):
+        return "Brak pliku"
 
-    with open(filename, mode ='r') as file:
-        lines = file.readlines()
 
-        if header:
-            header_list.append(lines[0].replace('\n',''))
-            return header_list,filename,sep
-    return header_list, filename, sep
 
 # Wypisanie etykiet – funkcja wypisująca etykiety lub komunikat, że etykiet nie było w danym datasecie
-def label(file):
-    if len(read_file(file, False)[0]) == 0:
+def label(file, header=True):
+    if  read_file(file,header,',')[3]==False:
         return 'Brak etykiet'
-    sep = read_file(file, False)[2]
-    return [lab for lab in read_file(file)[0].split(sep)]
+    # sep = read_file(file)[2]
+    return [lab for lab in read_file(file)[0]]
 
 # Wypisanie danych datasetu – funkcja wypisuje kolejne wiersze datasetu. Bez podania parametrów
 # wypisywany jest cały dataset, ale możliwe też podanie 2 parametrów, które określają przedział, który ma
@@ -50,6 +53,9 @@ def show_data(*args):
 # procentowo jaka część głównego zbioru danych trafia do poszczególnych zbiorów
 
 def training(file, tr=15,t=80,w=5):
+    tr = int(input('Podaj tr trening'))
+    t = int(input('Podaj t test'))
+    w = int(input('Podaj w walidacja'))
 
 
     file = read_file(file, True )[1]
@@ -110,15 +116,51 @@ def row_for_decide_clas(file, sep, value_clas):
 która może być podzbiorem datasetu, zmienną przechowującą dane treningowe, itp. 
 Dodatkowo podawana jest nazwa pliku, do którego dane zostaną zapisane.'''
 
+def inside_dataset(fileopen,filewrite, list_part=[]):
+    with open(fileopen, mode='r') as file:
+        lines = file.readlines()
+        no_line = int(input('Podaj nr końcowego wiersza '))
+        part_dataset = lines[:no_line]
+        for row in part_dataset:
+            list_part.append(row[:-2])
+    with open(filewrite, mode='w') as file:
+        for row in list_part:
+            file.write(row+'\n')
+    return list_part
+
+'''Menu programu'''
+
+while True:
+    print('w - wczytaj plik')
+    print('l - wypisanie etykiet')
+    print('s - Wypisanie danych datasetu')
+    print('p - Podział datasetu')
+    print('q - exit')
+
+    key = input('Podaj klucz z klawiatury:  ')
+    if key == 'w':
+        print(read_file('iris.csv', True))
+    if key == 'l':
+        print(label(read_file('iris.csv')[1]))
+    if key == 'l':
+        print(show_data('iris.csv'))
+    if key == 'p':
+        training('iris.csv')
+    if key == 'q':
+        break
+
+
+
+# print(inside_dataset('iris.csv','write.csv'))
 # print(name_decide_clas('iris.csv',','))
-print(row_for_decide_clas('iris.csv', ',', 'Iris-setosa'))
+# print(row_for_decide_clas('iris.csv', ',', 'Iris-setosa'))
 # print(count_decide_clas('iris.csv',','))
 # training('iris.csv', 1,2,1)
 # print(name_decide_clas('iris.csv',','))
 # print(training('iris.csv', 15,80,5)[0])
-# print(read_file('iris.csv',True)[1])
+# print(read_file('iris.csv',True))
 # show_data('iris.csv')
 
 
-# print(label(read_file('iris.csv',True)[1]))
+print(label(read_file('iris.csv',False)[1]))
 #
